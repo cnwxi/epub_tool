ddf8bdedcdfd18eecc655dcd3d41e7d2cda0a6ff
xiangyuwang <xiangyuwang.chn@qq.com>
2024-05-09 15:28:23 +0800

    update

--- pyinstaller.sh
+++ pyinstaller.sh
@@ -1,7 +1,7 @@
 pyinstaller -F 重构epub并反文件名混淆.py -n decrypt_epub; echo "------decrypt_epub finished------";
 pyinstaller -F 重构epub并加入文件名混淆.py -n encrypt_epub; echo "------encrypt_epub finished------";
-pyinstaller -F 重构epub为规范格式_v2.8.3.py -n rebuild_epub_v2.8.3; echo "------rebuild_epub finished------";
-rm ./build
+pyinstaller -F 重构epub为规范格式_v2.8.3.py -n rebuild_epub; echo "------rebuild_epub finished------";
+rm -rf ./build
 rm ./decrypt_epub.spec
 rm ./encrypt_epub.spec
 rm ./rebuild_epub_v2.8.3.spec
