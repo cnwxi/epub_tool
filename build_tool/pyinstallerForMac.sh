fe7e974541a0721e193698ad2b03664de7d6557d
WxyMac <xiangyuwang.chn@gmail.com>
2025-04-23 16:59:37 +0800

    调整不同系统编译操作

--- build_tool/pyinstallerForMac.sh
+++ build_tool/pyinstallerForMac.sh
@@ -1,5 +1,10 @@
 # for mac
 pyinstaller --noconfirm -w -i ./img/icon.ico -F Epub_Tool_TKUI.py -n Epub_Tool_TKUI;
+cd ./dist
+zip -r Epub_Tool_TKUI.zip ./Epub_Tool_TKUI.app;
+rm -rf ./Epub_Tool_TKUI.app;
+rm -rf ./Epub_Tool_TKUI;
 echo "------finished------";
+cd ..
 rm -rf ./build;
 rm ./Epub_Tool_TKUI.spec;
