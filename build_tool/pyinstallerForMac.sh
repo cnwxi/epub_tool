839a2b31ba8794a04be02ca2252d024c6356f9fe
WxyMac <xiangyuwang.chn@gmail.com>
2025-04-23 17:56:14 +0800

    移除命令行程序编译；移除mac编译-F参数；添加icon.icns图标适配macOS，优化显示效果；移除Ubuntu（Linux系统）编译。

--- build_tool/pyinstallerForMac.sh
+++ build_tool/pyinstallerForMac.sh
@@ -1,5 +1,5 @@
 # for mac
-pyinstaller --noconfirm -w -i ./img/icon.ico -F Epub_Tool_TKUI.py -n Epub_Tool_TKUI;
+pyinstaller --noconfirm -w -i ./img/icon.icns -F Epub_Tool_TKUI.py -n Epub_Tool_TKUI;
 cd ./dist
 zip -r Epub_Tool_TKUI.zip ./Epub_Tool_TKUI.app;
 rm -rf ./Epub_Tool_TKUI.app;
